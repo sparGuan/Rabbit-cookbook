b9cdf19d7b62a2c2eb50e9e829d4c93e35a6533e
Spar <493683987@qq.com>
2018-09-13 17:20:36 -0700

    commit

---
+++ server/src/middlewares/catchErrors.ts
@@ -0,0 +1,18 @@
+const assert = require('assert');
+/**
+ * 全局异常捕获
+ */
+module.exports =  () => {
+    return async (ctx :any, next: any) => {
+        try {
+            await next();
+        } catch (err) {
+            if (err instanceof assert.AssertionError) {
+                ctx.res = err.message;
+                return;
+            }
+            ctx.res = `Server Error: ${err.message}`;
+            console.error('Unhandled Error\n', err);
+        }
+    };
+};
---
+++ server/src/middlewares/enhanceContext.ts
@@ -0,0 +1,12 @@
+
+/**
+ * 增强context对象
+ */
+module.exports =  () => {
+  return async (ctx: any, next: any) => {
+      await next();
+      if (ctx.acknowledge) {
+          ctx.acknowledge(ctx.res);
+      }
+  };
+};
