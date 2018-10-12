12c55974cc7b6c8a2cf9495b49f2042026e143e2
root <493683987@qq.com>
2018-10-12 18:04:56 +0800

    commit

--- server/src/io/isLogin.connection.ts
+++ server/src/io/isLogin.connection.ts
@@ -6,9 +6,9 @@ export default (socket: any) => {
   // 判断是否登录，更新用户的socketId
   socket.on('isLogin', async (user: any) => {
     if (!global._.isEmpty(user)) {
-       // 登录之后把user放进socket对象，然后完事
-       // socket.sockets.sockets[socket.id].emit('message', 'for your eyes only');
-      const socket_schema: ISocket =  (await Socket.findOneAndUpdate(
+      // 登录之后把user放进socket对象，然后完事
+      // socket.sockets.sockets[socket.id].emit('message', 'for your eyes only');
+      const socket_schema: ISocket = (await Socket.findOneAndUpdate(
         { id: socket.id },
         {
           user,
@@ -18,39 +18,47 @@ export default (socket: any) => {
           new: true
         }
       )) as ISocket;
-      const userInfo: IUser = await User.findByIdAndUpdate(user._id, { $set: {
-        socket: socket_schema,
-        updateTime: new Date()
-      }}, {new: true}).select('-passWord -updateTime -logoutTime -createTime ')
-      .populate({ path: 'requestList', select: ' headImg nickName descPerson sex ' })
-      .populate({ path: 'friends', select: ' headImg nickName descPerson sex ' })
-      .exec() as IUser
-      const appId = wx.appId
+      const userInfo: IUser = (await User.findByIdAndUpdate(
+        user._id,
+        {
+          $set: {
+            socket: socket_schema,
+            updateTime: new Date()
+          }
+        },
+        { new: true }
+      )
+        .select('-passWord -updateTime -logoutTime -createTime ')
+        .populate({
+          path: 'requestList',
+          select: ' headImg nickName descPerson sex '
+        })
+        .populate({
+          path: 'friends',
+          select: ' headImg nickName descPerson sex '
+        })
+        .exec()) as IUser;
+      const appId = wx.appId;
       // 在这里生成token和ticket的签名,保存进数据库
       const wxToken = await getWxConfigUtil.getToken();
       // console.log(wxToken)
       const wxTicket = await getWxConfigUtil.getTicket(wxToken);
-      // console.log(wxTicket)
-      const nonceStr = getWxConfigUtil.createNonceStr();
-      // console.log(nonceStr)
-      const timestamp = getWxConfigUtil.createTimeStamp();
       // console.log(timestamp)
-      const signature = await getWxConfigUtil.calcSignature(
+      // url 为调用页面的完整 url
+      const wxConfig = await getWxConfigUtil.sign(
         wxTicket,
-        nonceStr,
-        timestamp,
         `${domain}${socket.request.url}` // 这个socketId代替url
         // ctx.request.url
-      ); // 获取签名       
-      console.log(wxTicket)   
-      console.log(nonceStr)  
-      console.log(timestamp)
-      console.log(`${domain}${socket.request.url}`)
+      ); // 获取签名
+      wxConfig.appId = appId
       // 将微信服务的签名返回到前端展示
       // console.log(`here is isLogin.conn 23 page ${userInfo}`)
       // 返回去的token码是不带socket值的
       // 返回用户信息好签名
-      socket.emit(`isLogin_${emit}`, {userInfo , wxConfig: {appId, timestamp, nonceStr, signature}});
+      socket.emit(`isLogin_${emit}`, {
+        userInfo,
+        wxConfig
+      });
     }
   });
 };
--- server/src/utils/getWxConfigUtil.ts
+++ server/src/utils/getWxConfigUtil.ts
@@ -2,7 +2,7 @@ import { resolve } from 'bluebird';
 const axios = require('axios');
 import { wx } from '../config/index';
 const jsSHA = require('jssha');
-const crypto = require('crypto')
+const crypto = require('crypto');
 /**
  * @default 获取微信js-sdk的token和ticket
  */
@@ -17,14 +17,14 @@ class WxConfigUtil {
           wx.appId
         }&secret=${wx.secret}`
       )
-      .then((res: any) => {        
-        this.access_token = res.data.access_token;        
+      .then((res: any) => {
+        this.access_token = res.data.access_token;
         resolve();
         // 这个异步回调里可以获取access_token
       })
       .catch((error: any) => {
         console.log(error);
-      });            
+      });
     if (!global._.isEmpty(this.access_token)) {
       return this.access_token;
     } else {
@@ -55,10 +55,29 @@ class WxConfigUtil {
   }
 
   // timestamp
-  public createTimeStamp(): number {
-    return Date.now()
+  public createTimestamp(): number {
+    return Number(new Date().getTime() / 1000);
   }
-  // 计算签名方法
+  /**
+   * @synopsis 签名算法
+   *
+   * @param jsapi_ticket 用于签名的 jsapi_ticket
+   * @param url 用于签名的 url ，注意必须动态获取，不能 hardcode
+   *
+   * @returns
+   */
+  public sign(jsapi_ticket: string, url: string) {
+    const ret: any = {
+      jsapi_ticket,
+      nonceStr: this.createNonceStr(),
+      timestamp: this.createTimestamp(),
+      url
+    };
+    const string = this.raw(ret);
+    const shaObj = new jsSHA(string, 'TEXT');
+    ret.signature = shaObj.getHash('SHA-1', 'HEX');
+    return ret;
+  }
   /**
    * @description 实现传入参数返回签名
    * @param {ticket} String ticket
@@ -66,56 +85,70 @@ class WxConfigUtil {
    * @param {ts} String 时间戳
    * @param {url} String 再传入调用该函数的url
    * 最终生成签名
-   */// 计算签名
-  // 调用方式// const signature = calcSignature(ticket, noncestr, timestamp, url);
-  public async calcSignature(
-    ticket: any,
-    noncestr: string,
-    ts: number,
-    url: string
-  ) {
-    const str =
-      'jsapi_ticket=' +
-      ticket +
-      '&noncestr=' +
-      noncestr +
-      '&timestamp=' +
-      ts +
-      '&url=' +
-      url;
-      const shaObj = await new jsSHA(str, 'TEXT');
-      return shaObj.getHash('SHA-1', 'HEX');
+   */
+  public raw(args: any) {
+    let keys = Object.keys(args);
+    keys = keys.sort();
+    const newArgs: any = {};
+    keys.forEach((key: any) => {
+      newArgs[key.toLowerCase()] = args[key];
+    });
+
+    let string = '';
+    for (const k in newArgs) {
+      string += '&' + k + '=' + newArgs[k];
+    }
+    string = string.substr(1);
+    return string;
   }
+  // public async calcSignature(
+  //   ticket: any,
+  //   noncestr: string,
+  //   ts: number,
+  //   url: string
+  // ) {
+  //   const str =
+  //     'jsapi_ticket=' +
+  //     ticket +
+  //     '&noncestr=' +
+  //     noncestr +
+  //     '&timestamp=' +
+  //     ts +
+  //     '&url=' +
+  //     url;
+  //   const shaObj = await new jsSHA(str, 'TEXT');
+  //   return shaObj.getHash('SHA-1', 'HEX');
+  // }
   public async sha1(str: string) {
-    const md5sum = await crypto.createHash("sha1");
+    const md5sum = await crypto.createHash('sha1');
     await md5sum.update(str);
-    return md5sum.digest("hex");    
+    return md5sum.digest('hex');
   }
   /**
    *  @param nonce,timestamp,signature 如果微信发过来的跟自己的匹配的上的话
    *  输出
    */
-  public async validateToken(query: any) {    
+  public async validateToken(query: any) {
     // console.log("*** URL:" + req.url);
     // console.log(query);
     const echostr = query.echostr;
-    const signature = query.signature;    
-    const timestamp = query['timestamp'];
+    const signature = query.signature;
+    const timestamp = query.timestamp;
     const nonce = query.nonce;
     const oriArray = new Array();
     oriArray[0] = nonce;
     oriArray[1] = timestamp;
-    oriArray[2] = wx.token; //微信开发者中心页面里填的token
+    oriArray[2] = wx.token; // 微信开发者中心页面里填的token
     oriArray.sort();
     const original = oriArray.join('');
-    console.log("Original str : " + original);
-    console.log("Signature : " + signature);
+    console.log('Original str : ' + original);
+    console.log('Signature : ' + signature);
     const scyptoString = await this.sha1(original);
     if (signature === scyptoString) {
       // 返回铭文
-      return echostr
+      return echostr;
     } else {
-        return signature
+      return signature;
     }
   }
 }
