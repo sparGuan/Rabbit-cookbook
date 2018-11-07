3f40008b69f0bcdcb0f661e96e99782f81d98818
root <493683987@qq.com>
2018-11-07 20:40:57 +0800

    commit

--- src/views/homepackage/screenshop/src/vendor/AppDetilPage.js
+++ src/views/homepackage/screenshop/src/vendor/AppDetilPage.js
@@ -17,6 +17,14 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import AppSlide from './AppSlide';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import tileData from './tileData';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import CardMedia from '@material-ui/core/CardMedia';
+import { DefaultPlayer as Video } from 'react-html5video';
+import 'react-html5video/dist/styles.css';
+import AppBar from '@material-ui/core/AppBar';
 const styles = theme => ({
   card: {
     width: '100%',
@@ -62,7 +70,10 @@ const styles = theme => ({
     position:'relative',
   },
   reviewExpand: {
-    padding:'0 10px'
+    padding:'0 10px',
+    position: 'absolute',
+    right: 0,
+    top: -3,
   },
   drawBorderBottom: {
     borderBottom: '3px solid rgb(245, 245, 245)'
@@ -70,6 +81,43 @@ const styles = theme => ({
   showPrice: {
     position: 'absolute',
     right: 45,
+  },
+  gridList: {
+    margin:'0 !important',
+    width: '100%',
+    height: 'auto',
+  },
+  cellPanel: {
+    position:'relative'
+  },
+  cover: {
+    width: '100%'
+  },
+  cardSmallSpacing: {
+    padding:5
+  },
+  cardExceptSpacing: {
+    padding:'0 !important'
+  },
+  disableVideoView:{
+    borderRadius: 0,
+    boxShadow: 'unset'
+  },
+  videoHeight: {
+    height:205,
+    overflow:'hidden',
+    '& > video': {
+      objectFit: 'cover',
+      objectPosition: 'center center'
+    }
+  },
+  detailGridListItem: {
+    '&>div': {
+      borderRadius:5
+    }
+  },
+  rowExpandedSwitch: {
+    height: '25px'
   }
 });
 
@@ -118,7 +166,8 @@ class RecipeReviewCard extends React.Component {
                   slideChangeTransitionStart={this.slideChangeTransitionStart}
                   slideChangeTransitionEnd={this.slideChangeTransitionEnd}
         />
-        <CardContent>
+
+        <CardContent className={classes.cellPanel}>
           <Typography component="p">
             This impressive paella is a perfect party dish and a fun meal to cook together with your
             guests. Add 1 cup of frozen peas along with the mussels, if you like.
@@ -138,17 +187,21 @@ class RecipeReviewCard extends React.Component {
           </IconButton>
         </CardActions>
         <CardActions className={classnames(classes.actions,classes.unsetSpacing,classes.relativeAuto)} disableActionSpacing>
-              <span className={classes.ariaTitle}>参数列表</span>
-               <IconButton
-                  className={classnames(classes.expand, classes.reviewExpand,{
-                    [classes.expandOpen]: this.state.expanded,
-                  })}
-                  onClick={this.handleExpandClick}
-                  aria-expanded={this.state.expanded}
-                  aria-label="Show more"
-                >
-                  <ExpandMoreIcon />
-              </IconButton>
+              <div onClick={this.handleExpandClick}
+                    aria-expanded={this.state.expanded}
+                    aria-label="Show more"
+                    className={classes.rowExpandedSwitch}
+              >
+                <span className={classes.ariaTitle}>参数 / 详情</span>
+                <IconButton
+
+                    className={classnames(classes.expand, classes.reviewExpand,{
+                      [classes.expandOpen]: this.state.expanded,
+                    })}
+                  >
+                    <ExpandMoreIcon />
+                </IconButton>
+              </div>
         </CardActions>
         {
           // unmountOnExit
@@ -163,6 +216,46 @@ class RecipeReviewCard extends React.Component {
               minutes.
             </Typography>
           </CardContent>
+          {
+          // 图文详情
+          // 商品详情
+          // 视频
+          }
+          <Typography component="div"  gutterBottom>
+            <Card  className={classnames(classes.cardSmallSpacing,classes.disableVideoView)}>
+                <CardContent className={classes.cardExceptSpacing}>
+                    <CardMedia
+                      component={
+                        () => 
+                        (<Video autoPlay loop muted
+                            controls={['PlayPause', 'Seek', 'Time', 'Volume', 'Fullscreen']}
+                            poster="http://sourceposter.jpg"
+                            className={classes.videoHeight}
+                            onCanPlayThrough={() => {
+                                // Do stuff
+                            }}>
+                            <source 
+                            src="https://tbm-auth.alicdn.com/VIIlksjrCptwSqsJDKZ/p4nNuYRovXp4xws8uZP@@hd_hq.mp4?auth_key=1541579090-0-0-8e4b66b2e5575957a526085ab4f7f6c9" type="video/webm" />
+                            <track label="English" kind="subtitles" srcLang="cn" src="http://source.vtt" default />
+                        </Video>)
+                      }
+                      className={classes.cover}
+                      title="Live from space album cover"
+                    />
+                </CardContent>
+            </Card>
+            {
+              // 原图
+            }
+            <GridList cellHeight={160} spacing={30} className={classes.gridList} >
+            {tileData.map((tile,index) => (
+              index < 4 && 
+              <GridListTile key={tile.img} cols={2} rows={2} className={classes.detailGridListItem}>
+                <img src={tile.img} alt={tile.title} />
+              </GridListTile>
+            ))}
+            </GridList>
+          </Typography>
         </Collapse>
       </Card>
     );
--- src/views/homepackage/screenshop/src/vendor/AppGridList.js
+++ src/views/homepackage/screenshop/src/vendor/AppGridList.js
@@ -7,7 +7,6 @@ import GridListTileBar from '@material-ui/core/GridListTileBar';
 import IconButton from '@material-ui/core/IconButton';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
 import tileData from './tileData';
-// import InfoIcon from '@material-ui/icons/Info';
 import GridBuyAndAddCar from './GridBuyAndAddCar'
 const styles = theme => ({
   root: {
